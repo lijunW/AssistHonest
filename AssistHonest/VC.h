23d4094094a8d6be6aff7b4fa78c190dfffc3286
ZWQ123456789aaabbb <2055150889@qq.com>
2017-12-14 14:54:58 +0800

    给你新建了一个猪VC记得删了

---
+++ AssistHonest/VC.h
@@ -0,0 +1,13 @@
+//
+//  VC.h
+//  AssistHonest
+//
+//  Created by xiechuang on 2017/12/14.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VC : BaseViewController
+
+@end
---
+++ AssistHonest/VC.m
@@ -0,0 +1,13 @@
+//
+//  VC.m
+//  AssistHonest
+//
+//  Created by xiechuang on 2017/12/14.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import "VC.h"
+
+@implementation VC
+
+@end
