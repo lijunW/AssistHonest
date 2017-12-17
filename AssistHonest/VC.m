40ef44bac46bdf2f5bc8a30bbf88e2afc3cdf109
wanglijun <wanglijun@wanglijuns-MacBook-Pro.local>
2017-12-17 18:30:18 +0800

    我新建的button2

--- AssistHonest/VC.m
+++ AssistHonest/VC.m
@@ -15,7 +15,7 @@
     [super viewDidLoad];
     //这块都是我写的
     
-    UIButton *button = [UIButton new];
+    //UIButton *button = [UIButton new];
 
     //我又写了一点，写好了，先提交了
     NSLog(@"log1:%s",__func__);
@@ -23,6 +23,9 @@
     NSLog(@"log3:%s",__func__);
     
     UIButton *button = [UIButton new];
+    UIButton *button2 = [UIButton new];
+    
+    
     
     
 }
